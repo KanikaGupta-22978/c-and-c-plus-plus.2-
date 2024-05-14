838616f43c0aee57b98dc3d53d07287d8cc03a8c
Kanika Gupta <122543216+kanikaguptakk@users.noreply.github.com>
2024-05-14 10:12:22 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+// creating a basic C application
+#include <stdio.h> // standard input/output library
+#include <stdlib.h> // memory allocation function container
+
+int main() // inside it will only be executed
+{
+    // declare a variable
+    int value;
+    // read a value from the keyboard
+    printf("enter a number:");
+    scanf("%d", &value); // it put that integer into the memory address of value
+    // printing the number
+    printf("the value stored is : %d", value);
+    return 0;  // code is finished
+}
+
